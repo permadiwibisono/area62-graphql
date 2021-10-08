15dd18e0e30211dbc07427f37637ee0f79ee13e1
Permadi Wibisono <mbapewe@gmail.com>
2021-10-09 03:04:19 +0700

    Handle CountryInput for filter in country resolver

--- src/schema/resolvers/country.resolver.ts
+++ src/schema/resolvers/country.resolver.ts
@@ -1,13 +1,18 @@
-import { Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql"
+import { Arg, Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql"
+import { parseFilterInput } from "../../utils/query"
 import { GraphQLContext } from "../../types"
 import { Country } from "../entities/country.entity"
 import { Province } from "../entities/province.entity"
+import { CountryInput } from "../inputs/country.input"
 
 @Resolver(() => Country)
 export class CountryResolver {
   @Query(() => [Country!])
-  countries(@Ctx() { em }: GraphQLContext): Promise<Country[]> {
-    return em.find(Country, {})
+  countries(
+    @Arg("filter", { nullable: true }) filter: CountryInput,
+    @Ctx() { em }: GraphQLContext
+  ): Promise<Country[]> {
+    return em.find(Country, parseFilterInput<Country>(filter))
   }
 
   @Query(() => Number)
