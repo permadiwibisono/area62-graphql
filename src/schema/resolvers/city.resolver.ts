45d5efd9860d404dd19f254589e8631ece3d4caf
Permadi Wibisono <mbapewe@gmail.com>
2021-10-11 16:48:56 +0700

    Update filter in `city` resolver

--- src/schema/resolvers/city.resolver.ts
+++ src/schema/resolvers/city.resolver.ts
@@ -1,13 +1,26 @@
-import { Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql"
+import { Arg, Ctx, FieldResolver, Query, Resolver, Root } from "type-graphql"
+import { parseFilterInput } from "../../utils"
 import { GraphQLContext } from "../../types"
 import { City } from "../entities/city.entity"
 import { Province } from "../entities/province.entity"
+import { CityInput } from "../inputs/city.input"
 
 @Resolver(() => City)
 export class CityResolver {
   @Query(() => [City!])
-  cities(@Ctx() { em }: GraphQLContext): Promise<City[]> {
-    return em.find(City, {})
+  cities(
+    @Arg("filter", { nullable: true }) filter: CityInput,
+    @Ctx() { em }: GraphQLContext
+  ): Promise<City[]> {
+    return em.find(City, parseFilterInput<City>(filter))
+  }
+
+  @Query(() => City)
+  city(
+    @Arg("code") code: string,
+    @Ctx() { em }: GraphQLContext
+  ): Promise<City> {
+    return em.findOneOrFail(City, { code })
   }
 
   @Query(() => Number)
